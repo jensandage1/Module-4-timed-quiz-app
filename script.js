ee60292d6b4e0059ccb6da729ab9cae4d45c93b5
Jen Sandage <jensandage1@gmail.com>
2023-06-26 18:30:45 -0700

    styling changes, start screen div disappears when first question appears

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ var questionsArr = [question1, question2, question3, question4];
 
 var question1 = "What does HTML stand for?";
 var answer1 = ["hypertext markup language", "something else", "another thing"];
+var correct1 = "hypertext markup language";
 var question2 = "What does CSS stand for?";
 var answer2 = ["cascading style sheets", "it doesnt", "dunno"];
+var correct2 = "cascading style sheets";
 var question3 = "Is Java the same as JavaScript?";
 var answer3 = ["true", "false", "there arent any other options"];
+var correct3 = "false";
 var question4 = "What is the keyboard command to copy and item?";
 var answer4 = ["ctrl v" , "ctrl j", "ctrl 8", "ctrl jkl"];
+var correct4 = "ctrl v";
 
 
 
@@ -35,9 +39,29 @@ function showQuestions () {
     answersList.appendChild(li);
   }
 
-}
+var correctWrong = document.getElementById("correct-wrong");
+var counter = document.getElementById("counter");
+answersList.addEventListener("click", rightWrong);
+
+function rightWrong () {
+  /*if ( WHAT GOES HERE?!?!?!?? ) { 
+    correctWrong.textContent = "Correct";
+    counter++;
+    //display correct add counter.
+  } else {
+    correctWrong.textContent = "Wrong";
+    //display wrong.
+  }*/
+}}
 
 startQuizBtn.addEventListener("click", showQuestions); //works, div show on button click. 
+startQuizBtn.addEventListener("click", hideStartScreen);
+
+function hideStartScreen () {
+  document.querySelector(".start-screen").style.display = "none";
+}
+
+
 
 var timerDiv = document.getElementById("timer-div");
 var timer = document.querySelector(".timer");
